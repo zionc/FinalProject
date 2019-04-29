f9e8b4e8834bb97a1639f34b3fb61fc258ac5aeb
Miguel Oyler-Castrillo <33669627+migueloyler@users.noreply.github.com>
2019-04-29 12:09:44 -0400

    Update RankPageScrape.py
    changed the scraper so it could also gather the team Rank.

--- RankPageScrape.py
+++ RankPageScrape.py
@@ -7,6 +7,10 @@ from bs4 import BeautifulSoup
 import bs4
 import requests
 import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 def extract_source(url):
      agent = {"User-Agent":"Mozilla/5.0"}
@@ -24,10 +28,12 @@ def getTeamNames(source, region=''):
 def getRankPairs(source, region=''):
      soup=bs4.BeautifulSoup(source, 'lxml')
      names= soup.findAll('td', {'class' : re.compile('points')})
+     ranks = soup.findAll('td', {'class' : re.compile('fi-table__td fi-table__rank$')})
      regions = soup.findAll('span', {'class' : re.compile('text')})
      regionList = []
      currPtsList = []
      prevPtsList = []
+     rankList = []
      cnt = 0
      for i in names:
          if cnt % 2 == 0:
@@ -39,9 +45,11 @@ def getRankPairs(source, region=''):
      for i in regions:
          if '#' in i.get_text():
              regionList.append(i.get_text())
+     for i in ranks:
+         rankList.append(i.get_text())
      ptsTupleList = []
-     for i, j, k in zip(currPtsList, prevPtsList, regionList):
-         ptsTupleList.append((int(i.encode('utf-8')),int(j.encode('utf-8')), k))
+     for i, j, p, q in zip(currPtsList, prevPtsList, regionList, rankList):
+         ptsTupleList.append((int(i.encode('utf-8')),int(j.encode('utf-8')), p, int(q)))
      return ptsTupleList
          
 def generateDict(teamList, tupleList):
@@ -50,7 +58,7 @@ def generateDict(teamList, tupleList):
         team_dict[teamList[i]] = tupleList[i]
     return team_dict
 
-region = '#UEFA'
+
 
 teamList = getTeamNames(extract_source('https://www.fifa.com/fifa-world-ranking/ranking-table/men/'))
 teamRank = getRankPairs(extract_source('https://www.fifa.com/fifa-world-ranking/ranking-table/men/'))
@@ -59,4 +67,4 @@ f = open('TeamInfo.txt', 'w')
 
 teamDict = generateDict(teamList, teamRank)
 for k, v in teamDict.items():
-    f.write("{0} \n {1} \n {2} \n {3} \n".format(k, v[0], v[1], v[2]))
+    f.write("{0} \n {1} \n {2} \n {3} \n {4} \n".format(k, v[3], v[0], v[1], v[2]))
